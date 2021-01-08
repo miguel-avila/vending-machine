d68cb3190df810add287145f0255e2587aec292e
Robert <robertlewiscook@gmail.com>
2021-01-08 13:28:04 -0500

    adding for Miguel

--- module-1/19_Capstone/java/src/main/java/com/techelevator/Item.java
+++ module-1/19_Capstone/java/src/main/java/com/techelevator/Item.java
@@ -1,70 +1,48 @@
 package com.techelevator;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+
+ 
 
 public class Item {
-	Map<String, Integer> inventoryItems = new HashMap<String, Integer>();
+private int quantity;
+private BigDecimal price;	
+private String name;
+
+
+
+//getters and setters
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getQuantity() {
+	return quantity;
+}
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
+public BigDecimal getPrice() {
+	return price;
+}
+public void setPrice(BigDecimal price) {
+	this.price = price;
+}
+
 	
 	
 	
 	
-	public static void main(String[] args) {
-		
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
-		// list or map of items (ex: candy
-		// or map of inventory slots
-
-		// create parent class item
-
-		// create methods prior to loading CSV (ex: Item, Price,
-
-		// hold name, slot numbers, create var for price
-
-		//
-
-		// keep track of our inventory
-		
-		// load up our inventory
-		inventoryItems.put("Beverages", 0);
-		inventoryItems.put("Candy", 5);
-		inventoryItems.put("Chips",5);
-		inventoryItems.put("Gum", 5);
-
-		// Prints the quantity of items in our inventory
-		// System.out.println("We have " + inventoryItems.get("Beverages") + " Beverages
-		// in stock");
-		// System.out.println("We have " + inventoryItems.get("Candy") + " Candy in
-		// stock");
-		// System.out.println("We have " + inventoryItems.get("Chips") + " Chips in
-		// stock ");
-		// System.out.println("We have " + inventoryItems.get("Gum") + " Gum in stock");
-
-		// if (inventoryItems.containsValue(0)) &&
-		// (inventoryItems.containsKey("Candy")){
-		// System.out.println("Candy is Sold out");
-
-		// Checks inventory and displays quantity
-		if (inventoryItems.containsKey("Beverages")) {
-			System.out.println("We have " + inventoryItems.get("Beverages") + " Beverages");
-		}
-		if (inventoryItems.containsKey("Candy")) {
-			System.out.println("We have " + inventoryItems.get("Candy") + " Candy");
-		}
-		if (inventoryItems.containsKey("Gum")) {
-			System.out.println("We have " + inventoryItems.get("Gum") + " Gum");
-		}
-
-		if (inventoryItems.containsKey("Chips")) {
-			if (inventoryItems.get("Chips").equals(0)) {
-				System.out.print("Chips Sold out");
-			}
-
-			else {
-				System.out.println("We have " + inventoryItems.get("Chips") + " Chips");
-			}
-		}
-
-	}
 }
